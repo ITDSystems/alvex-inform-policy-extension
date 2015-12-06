5123f624072ff2ba2c1cfe6a96bff6a2bfd0bebe
Alexey Vasyukov <vasyukov@gmail.com>
2015-12-06 15:36:18 +0300

    Logger messages done more readable

--- src/main/java/com/itdhq/inform/policy/InformPolicy.java
+++ src/main/java/com/itdhq/inform/policy/InformPolicy.java
@@ -97,9 +97,11 @@ public class InformPolicy
         fortemplate.put("lasteditorname", lasteditorname);
         fortemplate.put("creatorname", creatorname);
 
+        logger.debug("Notifying users about new version of the document");
+
         // Version creator
         if (creator) {
-            logger.debug("creator");
+            logger.debug("Notifying creator of the document");
             NodeRef mailCreatorTemplate = getMailTemplate(templates.get("creator"));
             sendMail(creatorname, mailCreatorTemplate, fortemplate);
             informedUsers.add(creatorname);
@@ -107,7 +109,7 @@ public class InformPolicy
 
         // Last editor
         if (lasteditor) {
-            logger.debug("lasteditor");
+            logger.debug("Notifying last editor of the document");
             if (!informedUsers.contains(lasteditorname)) {
                 NodeRef mailLastEditorTemplate = getMailTemplate(templates.get("lasteditor"));
                 sendMail(lasteditorname, mailLastEditorTemplate, fortemplate);
@@ -117,7 +119,7 @@ public class InformPolicy
 
         // All associated
         if (associated) {
-            logger.debug("associated");
+            logger.debug("Notifying users associated with the document");
             HashSet<String> associatedusernames = getAssociatedUsers(versionableNode);
             associatedusernames.removeAll(informedUsers);
             if (associatedusernames.size() > 0) {
@@ -132,7 +134,7 @@ public class InformPolicy
 
         // All editors
         if (editors) {
-            logger.debug("editors");
+            logger.debug("Notifying all previous editors of the document");
             HashSet<String> editornames = getEditors(versionableNode);
             editornames.removeAll(informedUsers);
             if (editornames.size() > 0) {
@@ -157,21 +159,21 @@ public class InformPolicy
 
     private String getDocumentCreator(NodeRef document)
     {
-        logger.debug("Inform owner");
+        logger.debug("Getting creator for the document");
         String owner = (String) nodeService.getProperty(document, ContentModel.PROP_CREATOR);
         return owner;
     }
 
     private String getLastEditor(Version version)
     {
-        logger.debug("Inform owner");
+        logger.debug("Getting last editor for the document");
         String editor = (String) version.getVersionProperty("creator");
         return editor;
     }
 
     private HashSet<String> getEditors(NodeRef document)
     {
-        logger.debug("Inform Authors");
+        logger.debug("Getting complete editors history for the document");
         VersionHistory versionHistory = versionService.getVersionHistory(document);
         ArrayList<Version> allVersions = new ArrayList(versionHistory.getAllVersions());
 
@@ -185,7 +187,7 @@ public class InformPolicy
 
     private HashSet<String> getAssociatedUsers(NodeRef versionableNode)
     {
-        logger.debug("Inform Associated Users");
+        logger.debug("Getting associated users for the document");
         HashSet <String> associatedUsers = new HashSet<>();
         ArrayList<AssociationRef> associationsTarget = (ArrayList<AssociationRef>) nodeService.getTargetAssocs(versionableNode, RegexQNamePattern.MATCH_ALL);
         for (AssociationRef assoc: associationsTarget)
@@ -200,7 +202,7 @@ public class InformPolicy
 
     private NodeRef getMailTemplate(String templatePATH) throws AlfrescoRuntimeException
     {
-        logger.debug("sendMails");
+        logger.debug("Getting mail templates from repository");
         ResultSet resultSet = serviceRegistry.getSearchService().query(new StoreRef(StoreRef.PROTOCOL_WORKSPACE, "SpacesStore") , SearchService.LANGUAGE_LUCENE, templatePATH);
         if (resultSet.length() == 0) {
             throw new AlfrescoRuntimeException("Can't find email template!");
@@ -210,7 +212,7 @@ public class InformPolicy
 
     private void sendMail(String username, NodeRef emailTemplateNodeRef, HashMap<String, Serializable> fortemplate) throws AlfrescoRuntimeException
     {
-        logger.debug("sendMail");
+        logger.debug("Sending notification to " + username);
         try {
             Action mailAction = actionService.createAction(MailActionExecuter.NAME);
             mailAction.setParameterValue(MailActionExecuter.PARAM_TEMPLATE, emailTemplateNodeRef);
