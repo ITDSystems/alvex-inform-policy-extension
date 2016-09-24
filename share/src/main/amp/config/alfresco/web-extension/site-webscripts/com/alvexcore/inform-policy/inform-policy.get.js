49d0fc683920acf0f224c114b996ea88a8f59ec1
azverev <azverev@itdhq.com>
2016-09-24 11:00:45 +0300

    Add blank share extension.

---
+++ share/src/main/amp/config/alfresco/web-extension/site-webscripts/com/alvexcore/inform-policy/inform-policy.get.js
@@ -0,0 +1,13 @@
+model.jsonModel = {
+    widgets: [{
+        id: "SET_PAGE_TITLE",
+        name: "alfresco/header/SetTitle",
+        config: {
+            title: "Inform policy configuration page."
+        }
+    },
+    {
+        id: "DEMO_SIMPLE_MSG",
+        name: "informPolicy/widgets/InformPolicyWidget"
+    }]
+};
---
+++ share/src/main/amp/web/js/informPolicy/widgets/InformPolicyWidget.js
@@ -0,0 +1,20 @@
+define(["dojo/_base/declare",
+        "dijit/_WidgetBase",
+        "alfresco/core/Core",
+        "dijit/_TemplatedMixin",
+        "dojo/text!./templates/InformPolicyWidget.html"
+    ],
+    function(declare, _Widget, Core, _Templated, template) {
+        return declare([_Widget, Core, _Templated], {
+            templateString: template,
+            i18nRequirements: [ {i18nFile: "./i18n/InformPolicyWidget.properties"} ],
+            cssRequirements: [{cssFile:"./css/InformPolicyWidget.css"}],
+            
+            buildRendering: function informPolicy_widgets_InformPolicyWidget__buildRendering() {
+                this.greeting = this.message('hello-label');
+
+                this.inherited(arguments);
+
+            }
+        });
+});
